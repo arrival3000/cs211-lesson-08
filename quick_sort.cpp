0385da2daefd378cbc07e135d4b3b375bf4102be
Naseka Andrew <duha.an.ru@yandex.ru>
2014-10-05 12:01:54 +0400

    Быстрая сортировка использует функцию partition, чтобы разделить массив на две части.
    В основной функции используется каскадная рекурсия, которая один вызов использует от 0
    
     до индекса полученного в partition, а вторая от индекса на единицу больше и до конца.

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -10,19 +10,56 @@ using std::swap;
 // заданной отрезком [left, right), так что в начале
 // следуют элементы меньшие pivot, а в конце - большие;
 // возвращает место начала блока элементов, больших pivot;
-int * partition(int * left, int *  right, int pivot) {
-	int * store = left; // место для вставки элементов, меньших pivot
-	for (int * p = left; p != right; ++p)
-		if (*p < pivot)
-			swap(*p, *store++);
-	return store;
+int partition(int *a, int l, int r, int pivot) {
+	int i = l - 1;
+	int j = r + 1;
+
+	while (true)
+	{
+	    do
+	    {
+		i++;
+	    }
+	    while (a[i] < pivot);
+
+	    do
+	    {
+		j--;
+	    }
+	    while (a[j] > pivot);
+
+	    if (i < j)
+		swap(a[i], a[j]);
+	    else
+	        return j;
+	}
 }
  
-void my_qsort(int * arr, int n) {
-	if (n <= 1)
-		return; // массив в 1 или 0 элементов уже упорядочен
-	int * pivotPtr = arr + rand() % n; // случайный выбор опорного элемента
-	int newPivotIdx = partition(arr, arr + n, *pivotPtr) - arr;
-	my_qsort(arr, newPivotIdx + 1);
-	my_qsort(arr + newPivotIdx, n - (newPivotIdx + 1));
+void my_qsort(int * arr, int l, int r) {
+	if (l >= r)
+	    return;
+	
+	int pivot = rand() % (r - l) + l;
+	int j = partition(arr, l, r, pivot);
+	my_qsort(arr, l, j);
+	my_qsort(arr, j + 1, r);
+}
+
+void print_arr(int * arr, int size, char delim = ' ')
+{
+    for(int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << delim;
+    }
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+	const int n = 10;
+	int a[n] = { 4, 6, 7, 3, 8, 2, 1, 0, 3, 11 };
+	print_arr(a, n);
+	my_qsort(a, 0, n - 1);
+	print_arr(a, n);
 }
